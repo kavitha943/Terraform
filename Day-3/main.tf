b9c56fc277014a0f611471a9c61fc607726b909f
Kavitha Gudibandi <166821151+kavitha943@users.noreply.github.com>
2025-04-15 13:27:56 +0000

    add files for day-3

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -3,8 +3,13 @@ provider "aws" {
     region = "eu-north-1"
   
 }
-resource "aws_instance" "demo" {
-    ami = var.ami_value
-    instance_type = var.instance_type
+module "ec2_instance" {
+    #path for importing modules form git repo
+    #source = "git::https://github.com/username/terraform-zero-to-hero.git//Day-3/modules/ec2_instance?ref=main"
+    #path for modules which are present in same directory
+    source = "./modules/ec2_instance"
+    ami_value="ami-0274f4b62b6ae3bd5"
+    instance_type = "t3.micro"
   
 }
+
