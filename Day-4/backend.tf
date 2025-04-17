eff00a7b56247ebc0d803c0ad8ab5fc6cd10367a
Kavitha Gudibandi <166821151+kavitha943@users.noreply.github.com>
2025-04-17 14:50:04 +0000

    locking terraform state using dynamodb

--- Day-4/backend.tf
+++ Day-4/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "abhishek-s3-demo-xyz" # change this
-    key            = "abhi/terraform.tfstate"
+    bucket         = "kavitha-s3-demo-xyz" 
+    key            = "kavitha/backend-terraform.tfstate" #path of statefile in s3
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-lock"
--- Day-4/main.tf
+++ Day-4/main.tf
@@ -1,15 +1,15 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-north-1"
 }
 
-resource "aws_instance" "abhishek" {
+resource "aws_instance" "kavitha" {
   instance_type = "t2.micro"
   ami = "ami-053b0d53c279acc90" # change this
-  subnet_id = "subnet-019ea91ed9b5252e7" # change this
+  #subnet_id = "subnet-019ea91ed9b5252e7" # change this
 }
 
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "abhishek-s3-demo-xyz" # change this
+  bucket = "kavitha-s3-demo-xyz" #give bucket name  unique
 }
 
 resource "aws_dynamodb_table" "terraform_lock" {
